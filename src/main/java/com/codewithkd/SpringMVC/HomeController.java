5c05c15606be3332b1ff47d207ba2debbe9d5e25
Kaushal Tarpara <kaushaltarpara10702@gmail.com>
2023-06-22 10:00:09 +0530

    Modify add using ModelAndView

--- src/main/java/com/codewithkd/SpringMVC/HomeController.java
+++ src/main/java/com/codewithkd/SpringMVC/HomeController.java
@@ -1,7 +1,9 @@
 package com.codewithkd.SpringMVC;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,15 +18,38 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public String add(HttpServletRequest req) {
+	public ModelAndView add(@RequestParam("num1")int i,@RequestParam("num2")int j) {
 		
-		int i=Integer.parseInt(req.getParameter("num1"));
-		int j=Integer.parseInt(req.getParameter("num2"));
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("result.jsp");
+		
 		int num3=i+j;
+		mv.addObject("sum3",num3);
 		
-		HttpSession session =req.getSession();
-		session.setAttribute("sum3", num3);
 		
-		return "result.jsp";
+		return mv;
 	}
+	
+//	@RequestMapping("add")
+//	public String add(@RequestParam("num1")int i,@RequestParam("num2")int j,HttpSession session) {
+//		
+//		int num3=i+j;
+//		
+//		session.setAttribute("sum3", num3);
+//		
+//		return "result.jsp";
+//	}
+	
+//	@RequestMapping("add")
+//	public String add(HttpServletRequest req) {
+//		
+//		int i=Integer.parseInt(req.getParameter("num1"));
+//		int j=Integer.parseInt(req.getParameter("num2"));
+//		int num3=i+j;
+//		
+//		HttpSession session =req.getSession();
+//		session.setAttribute("sum3", num3);
+//		
+//		return "result.jsp";
+//	}
 }
